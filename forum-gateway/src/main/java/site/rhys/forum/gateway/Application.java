ae07efcc1d47c0de0878c395528e42b754841062
xrs4433@outllok.com <xrsovell5201314>
2018-05-03 16:16:50 +0800

    init

--- forum-gateway/src/main/java/site/rhys/forum/gateway/Application.java
+++ forum-gateway/src/main/java/site/rhys/forum/gateway/Application.java
@@ -1,8 +1,7 @@
-package site.rhys.forum.zuul;
+package site.rhys.forum.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
