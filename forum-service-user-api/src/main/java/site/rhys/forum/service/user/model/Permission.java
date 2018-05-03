8bd9f76bf836238f1d7c888a292aa458d2b72e10
xrs4433@outllok.com <xrsovell5201314>
2018-05-03 21:54:11 +0800

    完善配置

---
+++ forum-service-user-api/src/main/java/site/rhys/forum/service/user/model/Permission.java
@@ -0,0 +1,17 @@
+package site.rhys.forum.service.user.model;
+
+import lombok.Data;
+
+/**
+ * <p>Company: www.rhys.site</p>
+ *
+ * @author : Rhys Xia
+ * @version :1.0
+ * @since : 2018-05-03 18:21
+ */
+@Data
+public class Permission {
+
+    private Long id;
+
+}
