f3e7f572ca22c0130dd4bc325cfffe808cc2b6d4
RhysXia <xrslovell5201314>
2018-05-09 14:38:23 +0800

    init

---
+++ src/main/java/site/rhys/forum/forumserver/ForumServerApplication.java
@@ -0,0 +1,12 @@
+package site.rhys.forum.forumserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumServerApplication.class, args);
+    }
+}
